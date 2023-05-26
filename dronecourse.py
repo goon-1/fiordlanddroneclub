36ab734d8341f082a2fc77e842ef09635faf7809
goon-1 <93413818+goon-1@users.noreply.github.com>
2023-05-26 21:21:20 +1200

    updating dronecourse.py
    see the description on pre-release v0.1.1

--- dronecourse.py
+++ dronecourse.py
@@ -1,4 +1,5 @@
 import csv
+import os
 # this is a programme that updates the leaderboard of a drone racing club.
 
 # the first step is choosing what leaderboard you want to update, we need a system where you can choose what type of leaderboard you want to update.
@@ -8,19 +9,29 @@ import csv
 #tinywhoop1
 #tinywhoop2
 #tinywhoop3
+
 def formatting():
 	print("-"*20)
 
+# Get the directory of the Python script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the file path relative to the script directory
+file_name = "tinywhoop1.csv"
+tinywhoop_path = os.path.join(script_dir, file_name)
+
+
 def viewLeaderboard():
-	with open('tinywhoop1.csv') as csv_file:
+	with open(tinywhoop_path, encoding='utf-8') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		formatting()
 		for row in csv_reader:
 			print(f'{row[0]} {row[1]} raced a {row[2]} second time on course {row[3]}')
 			formatting()
+
 			
 def view_entriesUser(fname, lname):
-	with open('tinywhoop1.csv') as csv_file:
+	with open(tinywhoop_path, encoding='utf-8') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		print(fname, lname + "'s race times:")
 		formatting()
@@ -37,6 +48,7 @@ def view_entriesUser(fname, lname):
 		print("average time (in seconds): " + str(mean))
 		print("times raced: " + str(total_times))
 		print("total time racing (in minutes): " + str(round(total_time/60, 2)))
+
 				
 
 def main():
@@ -47,18 +59,24 @@ def main():
 	user_input = input()
 	
 	if user_input == "1":
-		viewLeaderboard()
-		
+		try:
+			viewLeaderboard()
+		except Exception as e:
+			print("Error message: ", str(e))
+
 	elif user_input == "2":
 		first = input("User's first name (e.g. Shaun): ")
 		formatting()
 		last = input("User's last name (e.g. Cantwell): ")
 		formatting()
-		view_entriesUser(first, last)
+		try:
+			view_entriesUser(first, last)
+		except Exception as e:
+			print("Sorry, I can't find that name in the our database. Make sure the first letter of both the first and last name are capitalised with no extra spaces at the end.")
 		
 	else:
-		print("Not a valid input rip")
-	
-		
-main()
+		print("You didn't enter a valid number, try again...")
+
+while True:
+	main()
 		
